e0bc6e330a9a7415c1f041ac6a86bddf3282687f
Yash Bavkar <yashbavkar26@gmail.com>
2026-02-27 16:34:50 +0530

    Fix bugs while deploying

--- backend/api.py
+++ backend/api.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[os.getenv("ALLOWED_ORIGINS", "*").split(",")],
+    allow_origins=os.getenv("ALLOWED_ORIGINS", "*").split(","),
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
